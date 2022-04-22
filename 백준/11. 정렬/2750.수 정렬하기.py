cdd6dec3a08379c08525c90d57194b54af38590e
ElAsJay <tjdwnwkd12371@gmail.com>
2022-04-23 00:30:50 +0900

    [추가] 수 정렬하기

--- 백준/11. 정렬/2750.수 정렬하기.py
+++ 백준/11. 정렬/2750.수 정렬하기.py
@@ -4,12 +4,7 @@ num=[]
 for _ in range(T):
     num.append(int(input()))
 
-for i in range(T):
-    for j in range(i+1, T):
-        if(num[i] > num[j]):
-            t = num[i]
-            num[i] = num[j]
-            num[j] = t
+num.sort()
 
 for i in range(T):
     print(num[i])
